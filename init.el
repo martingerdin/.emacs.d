a388cfdf9bf8e09a27000c5cd54904fc26b75875
Martin Gerdin <martingerdin@gmail.com>
2016-12-13 22:05:29 +0100

    added zenburn

--- init.el
+++ init.el
@@ -1,12 +1,46 @@
 ;; my emacs init file
 ;;; add package sources
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; customize frame
+;;; set up custom theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/zenburn-emacs/")
+(load-theme 'zenburn t)
+;;; start in fullscreen mode
+(toggle-frame-fullscreen)
+;;; inhibit startup screen
+(setq inhibit-startup-screen t)
+;;; remove menu bar
+(menu-bar-mode -1)
+;;; remove tool bar
+(tool-bar-mode -1)
+;;; remove scroll bar
+(toggle-scroll-bar -1) 
+;;; set fringe color to same as background
+(set-face-attribute 'fringe nil
+		    :foreground (face-foreground 'default)
+		    :background (face-background 'default))
+;;; set color of vertical window divider to same as theme background
+(set-face-attribute 'vertical-border nil 
+		    :foreground (face-background 'default))
+;;; change default font size
+(set-face-attribute 'default (selected-frame) :height 100) 
+
+;; add package repository
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
 ;;; list of packages installed
 ;; magit
 
-
 ;; custom key bindings
 (global-set-key (kbd "C-x g") 'magit-status)
 (setq mac-right-option-modifier 'mac-right-option) ; right option as option
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings)) ; move between windows with shift and arrow keys
+

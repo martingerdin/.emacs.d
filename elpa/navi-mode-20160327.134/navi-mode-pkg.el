df8ab47c48e52c10029d5c4e24de51f265eafc45
Martin Gerdin <martingerdin@gmail.com>
2017-02-26 13:47:33 +0100

    added navi-mode

---
+++ elpa/navi-mode-20160327.134/navi-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "navi-mode" "20160327.134" "major-mode for easy buffer-navigation" '((outshine "2.0") (outorg "2.0")) :url "https://github.com/tj64/navi")
